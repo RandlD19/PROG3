12750057f7d9b62c68da0264849a12ea6a61673a
RandlD19 <damijanrandl@gmail.com>
2022-03-14 17:34:36 +0100

    dodane vaje_5

---
+++ Vaje_5/Koliko_najvecjih/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace Koliko_najvecjih
+{
+    class Program
+    {
+        /// <summary>
+        /// Funkcija prešteje število največjih elementov v podani tabeli
+        /// </summary>
+        /// <typeparam name="T"> generični tip, pri katerem obstaja primerjanje </typeparam>
+        /// <param name="tab"> tabela </param>
+        /// <returns> vrne celo število </returns>
+        public static int Koliko_najvecjih<T>(T[] tab) where T : IComparable<T>
+        {
+            T najvecji = tab[0];
+            int koliko = 0;
+
+            for (int i = 1; i < tab.Length; i++)
+            {
+                if (tab[i].CompareTo(najvecji) > 0)
+                {
+                    najvecji = tab[i];
+                    koliko = 1;
+                }
+                else if (tab[i].CompareTo(najvecji) == 0)
+                {
+                    koliko++;
+                }
+            }
+            return koliko;
+        }
+
+
+        static void Main(string[] args)
+        {
+            // Testi za Koliko največjih:
+            try
+            {
+                Console.WriteLine("Testni primer 1:");
+                Console.WriteLine(Koliko_najvecjih(new int[] { 1, 3, 5, 5, 5 }));
+                Console.WriteLine(Koliko_najvecjih(new int[] { 1, 3, 5 }));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("Testni primer 2:");
+                Console.WriteLine(Koliko_najvecjih(new char[] { '1', '3', '5' }));
+                Console.WriteLine(Koliko_najvecjih(new char[] { '1', '3', '5', '5', '5' }));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("Testni primer 3:");
+                Console.WriteLine(Koliko_najvecjih(new string[] { "asdf", "asdf", "kjof" }));
+                Console.WriteLine(Koliko_najvecjih(new string[] { "asdf", "kjof" }));
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ Vaje_5/Zoprna_sedmica/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace Zoprna_sedmica
+{
+    class Program
+    {
+        /// <summary>
+        /// za dano tabelo celih števil vrne tabelo z dvema elementoma
+        /// (število števil, ki so deljiva s 7, in vsoto preostalih števil),
+        /// iz dane tabele pa pobriše vsa števila, ki so deljiva s 7.
+        /// </summary>
+        /// <param name="tab"> tabela celih števil </param>
+        /// <returns> vrne tabelo velikosti 2 </returns>
+        public static int[] PoisciVII(int[] tab)
+        {
+            int[] vrni = new int[] { 0, 0 };
+            foreach (int st in tab)
+            {
+                if (st % 7 == 0)
+                {
+                    vrni[0] += 1;
+                }
+                else
+                {
+                    vrni[1] += st;
+                }
+            }
+            return vrni;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("TESTNI PRIMERI: ");
+            try
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Testni primer 1:");
+                Console.WriteLine("Pričakovan rezultat: 0 : 0");
+                Console.WriteLine("Rezultat: " + String.Join(" : ", PoisciVII(new int[] { })));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Testni primer 2:");
+                Console.WriteLine("Pričakovan rezultat: 0 : 10");
+                Console.WriteLine("Rezultat: " + String.Join(" : ", PoisciVII(new int[] { 10 })));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Testni primer 3:");
+                Console.WriteLine("Pričakovan rezultat: 3 : 23");
+                Console.WriteLine("Rezultat: " + String.Join(" : ", PoisciVII(new int[] { 1, 7, 5, 6, 7, 14, 4, 5, 2 })));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            try
+            {
+                Console.WriteLine("---------------------------------");
+                Console.WriteLine("Testni primer 4:");
+                Console.WriteLine("Pričakovan rezultat: 3 : 0");
+                Console.WriteLine("Rezultat: " + String.Join(" : ", PoisciVII(new int[] { 0, 0, 0 })));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
