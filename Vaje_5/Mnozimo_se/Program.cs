dee10ce835ed65e91803571ef28de8e83f8455d5
RandlD19 <damijanrandl@gmail.com>
2022-03-17 22:26:18 +0100

    nove naloge

--- Vaje_5/Mnozimo_se/Program.cs
+++ Vaje_5/Mnozimo_se/Program.cs
@@ -1,12 +1,69 @@
 ﻿using System;
+using System.Data;
+using System.IO;
+
 
 namespace Mnozimo_se
 {
     class Program
     {
+        /// <summary>
+        /// Funkcija iz podanega niz prebere števec in imenovalec ulomka
+        /// </summary>
+        /// <param name="niz">niz ki predstavlja ulomek</param>
+        /// <returns>vrne tabelo velikosti dva [stevec, imenovalec]</returns>
+        static int[] Izlusci_stevec_in_imenovalec(string niz)
+        {
+            string stevec = "";
+            string imenovalec = "";
+            int i = 0;
+            int imenovalec_st;
+            while (i < niz.Length && niz[i] != '/')
+            {
+                stevec += niz[i];
+                i++;
+            }
+            i++;
+            while (i < niz.Length)
+            {
+                imenovalec += niz[i];
+                i++;
+            }
+            int stevec_st = int.Parse(stevec);
+            if (imenovalec == "")
+            {
+                imenovalec_st = 1;
+            }
+            else
+            {
+                imenovalec_st = int.Parse(imenovalec);
+            }
+
+
+            return new int[2] { stevec_st, imenovalec_st };
+        }
+
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Za funkcijo eval
+            DataTable dt = new DataTable();
+            string dat = "stevila.txt";
+            StreamReader branje = File.OpenText(dat);
+            string vrstica = branje.ReadLine();
+            int stevec = 1;
+            int imenovalec = 1;
+            stevec *= Izlusci_stevec_in_imenovalec(vrstica)[0];
+            imenovalec *= Izlusci_stevec_in_imenovalec(vrstica)[1];
+            while ((vrstica = branje.ReadLine()) != null)
+            {
+                stevec *= Izlusci_stevec_in_imenovalec(vrstica)[0];
+                imenovalec *= Izlusci_stevec_in_imenovalec(vrstica)[1];
+                Console.WriteLine(stevec);
+                Console.WriteLine(imenovalec);
+            }
+            branje.Close();
+            Console.WriteLine($"{stevec}/{imenovalec}");
         }
     }
 }
---
+++ Vaje_5/Viva_La_Difference/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Viva_La_Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string zmanjsevanec;
+            string odstevanec;
+            double zmanj_d;
+            double odst_d;
+            try
+            {
+                Console.Write("Zmanjševanec: ");
+                zmanjsevanec = Console.ReadLine();
+                zmanj_d = double.Parse(zmanjsevanec);
+                if (zmanj_d < 0)
+                {
+                    throw new Exception("Zmanjševanec mora biti nenegativno število!");
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Zmanjševanec mora biti celo število!");
+                return;
+            }
+
+            try
+            {
+                Console.Write("Odštevanec: ");
+                odstevanec = Console.ReadLine();
+                odst_d = double.Parse(odstevanec);
+                if (odst_d < 0)
+                {
+                    throw new Exception("Odštevanec mora biti nenegativno število!");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Odštevanec mora biti celo število!");
+                return;
+            }
+
+            if (zmanj_d - odst_d < 0)
+            {
+                throw new Exception("Odštevanec mora biti manjši od zmanjševanca!");
+            }
+            Console.WriteLine(zmanj_d - odst_d);
+        }
+    }
+}
