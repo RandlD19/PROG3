9b3e687f3e7d0ee8fab528e2848fa804ff003fc5
RandlD19 <damijanrandl@gmail.com>
2022-03-27 12:47:04 +0200

    dodan del vaj, ki smo ga resili na faksu

---
+++ Vaje_21_3/Vaje_21_3/Class1.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Vaje_21_3
+{
+    public class Vozilo
+    {
+        private double gorivo;
+        private double kapaciteta;
+        private double poraba;
+        private double PreostaliKilometri;
+        private bool konstruktor = false;
+        public Vozilo(double kapaciteta, double poraba)
+        {
+            NastaviGorivo(kapaciteta);
+            NastaviKapaciteto(kapaciteta);
+            NastaviPorabo(poraba);
+            konstruktor = true;
+            NastaviPreostaliKilometri();
+
+
+
+        }
+
+        //public double Gorivo
+        //{
+        //    get { return this.gorivo; }
+        //    set
+        //    { if (value < 0)
+        //        {
+        //            this.gorivo = 0;
+        //        }
+        //        else
+        //        {
+        //            this.gorivo = value;
+        //        }
+
+        //    }
+        //}
+        public void NastaviKapaciteto(double vrednsot)
+        {
+            if (vrednsot < 0)
+            {
+                throw new ArgumentException("Neveljavni vnos!");
+            }
+            else
+            {
+                this.kapaciteta = vrednsot;
+
+            }
+        }
+
+        public void NastaviGorivo(double vrednsot)
+        {
+            if (vrednsot < 0)
+            {
+                throw new ArgumentException("Neveljavni vnos!");
+            }
+            else
+            {
+                this.gorivo = vrednsot;
+                NastaviPreostaliKilometri();
+            }
+        }
+        public void NastaviPorabo(double vrednsot)
+        {
+            if (vrednsot <= 0)
+            {
+                throw new ArgumentException("Neveljavni vnos!");
+            }
+            else
+            {
+                this.poraba = vrednsot;
+                NastaviPreostaliKilometri();
+
+
+            }
+        }
+        public void NastaviPreostaliKilometri()
+        {
+            if (konstruktor)
+            {
+                this.PreostaliKilometri = (this.gorivo / this.poraba) * 100;
+            }
+
+        }
+
+        public void Crpalka()
+        {
+            NastaviGorivo(this.kapaciteta);
+        }
+
+
+        public bool pot(double[] tab)
+        {
+            double zacetno_gorivo = this.gorivo;
+            
+            foreach (double v in tab)
+            {
+                if (v < 0)
+                {
+                    NastaviGorivo(zacetno_gorivo);
+                    throw new ArgumentException("Pot ne more biti negativna");
+                }
+                if (v == 0)
+                {
+                    if (this.gorivo == this.kapaciteta)
+                    {
+                        NastaviGorivo(zacetno_gorivo);
+                        throw new ArgumentException("Ne moremo tankati pri polnem tanku");
+                    }
+                    else
+                    {
+                        Crpalka();
+                    }
+                }
+                else
+                {
+                    try
+                    {
+                        NastaviGorivo(this.gorivo - (v * poraba) / 100);
+                    }
+                    catch
+                    {
+                        NastaviGorivo(zacetno_gorivo);
+                        return false;
+                    }
+
+                }
+            }
+            return true;
+
+
+           }
+        public double Gorivo()
+        {
+            return this.gorivo;
+        }
+        public double Kapaciteta()
+        {
+            return this.kapaciteta;
+        }
+        public double Poraba()
+        {
+            return this.poraba;
+        }
+
+    }
+}
---
+++ Vaje_21_3/WebClient_vaja/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+namespace WebClient_vaja
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            WebClient client = new WebClient();
+            client.Encoding = System.Text.Encoding.UTF8;
+            string vsebina = client.DownloadString("http://bos.zrc-sazu.si/sbsj.html");
+
+            string[] besede1 = vsebina.Split(new string[] { "<br>" }, StringSplitOptions.None);
+            Console.WriteLine(besede1);
+            List<string> ts = besede1.ToList();
+            for (int i = 0; i < 5; i++)
+            {
+                ts.RemoveAt(0);
+                ts.RemoveAt(ts.Count - 1);
+            }
+            List<string> besede = new List<string>();
+            foreach (string s in ts)
+            {
+                besede.Add(s.Substring(2, s.Length - 2));
+            }
+            Console.WriteLine(besede[0]);
+            Console.WriteLine(besede[ts.Count - 1]);
+
+            string vnos = Console.ReadLine();
+            foreach (string s in besede)
+            {
+                try
+                {
+                    if (s.Substring(0, vnos.Length) == vnos)
+                    {
+                        Console.WriteLine(s);
+                    }
+
+                }
+                catch { }
+
+
+            }
+        }
+    }
+}
+
