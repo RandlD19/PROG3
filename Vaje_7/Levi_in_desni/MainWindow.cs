b3b05488b77b6842056af08f4768de1ecc70dec4
RandlD19 <damijanrandl@gmail.com>
2022-04-02 21:05:53 +0200

    Dodan grafični vmesnik

---
+++ Vaje_7/Levi_in_desni/MainWindow.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void LeviPritisnjen(object sender, EventArgs e)
+    {
+        this.label2.Text = "<- pritisnjen";
+    }
+
+    protected void DesniPritinjen(object sender, EventArgs e)
+    {
+        this.label2.Text = "pritisnjen ->";
+    }
+
+    protected void LeviSpuscen(object sender, EventArgs e)
+    {
+        this.label2.Text = "";
+    }
+
+    protected void DesniSpuscen(object sender, EventArgs e)
+    {
+        this.label2.Text = "";
+    }
+}
---
+++ Vaje_7/Stevec_klikov/MainWindow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    int i = 0;
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    protected void Gumb_klik(object sender, EventArgs e)
+    {
+        i++;
+        this.button6.Label = $"Število klikov {i}";
+    }
+}
